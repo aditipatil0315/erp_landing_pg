f0be457cf687c3425a007b8c5a5ce7b1c63e0b06
Aditi Patil <aditiprakashpatil7@gmail.com>
2025-03-01 22:27:42 +0530

    final

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -17,7 +17,7 @@ const VideoSection = () => {
       <h2>Transform Your Business with Custom Solutions</h2>
       <div className="video-container" onClick={toggleMute}>
         <video ref={videoRef} autoPlay loop muted playsInline>
-          <source src="/videos/demo.MP4" type="video/mp4; codecs=avc1.42E01E, mp4a.40.2"  />
+          <source src="/videos/demo_2.mp4" type="video/mp4; codecs=avc1.42E01E, mp4a.40.2"  />
           Your browser does not support the video tag.
         </video>
         <div className="sound-indicator">{isMuted ? "🔇 Click for Sound" : "🔊 Sound On"}</div>
